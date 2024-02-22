1370790803b6e14763130c92abe811c2a5eab3ff
epsilone_not <epsilone_not@Lenevo>
2024-02-22 18:51:33 +0600

    First Commit

---
+++ src/pstu/hall/management/system/Login_page.java
@@ -0,0 +1,178 @@
+
+
+package pstu.hall.management.system;
+import javax.swing.JOptionPane;
+
+public class Login_page extends javax.swing.JFrame {
+
+    
+    public Login_page() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jCTextField1 = new app.bolivia.swing.JCTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jCTextField2 = new app.bolivia.swing.JCTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        rSMaterialButtonCircle1 = new rojerusan.RSMaterialButtonCircle();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(102, 102, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pstu/hall/management/system/rsz_download (1).png"))); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 10, 20, 20));
+
+        jLabel2.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Username :");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 190, 110, -1));
+
+        jLabel3.setFont(new java.awt.Font("SansSerif", 1, 30)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Welcome  To PSTU !");
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 70, 310, -1));
+
+        jLabel4.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Login To Your Acoount");
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 110, 200, -1));
+
+        jCTextField1.setBackground(new java.awt.Color(102, 102, 255));
+        jCTextField1.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(0, 0, 0)));
+        jCTextField1.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
+        jCTextField1.setPhColor(new java.awt.Color(255, 255, 255));
+        jCTextField1.setPlaceholder("Enter Username");
+        jCTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCTextField1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jCTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 220, 330, -1));
+
+        jLabel5.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Passward");
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 270, 110, -1));
+
+        jCTextField2.setBackground(new java.awt.Color(102, 102, 255));
+        jCTextField2.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(0, 0, 0)));
+        jCTextField2.setFont(new java.awt.Font("Noto Sans", 0, 14)); // NOI18N
+        jCTextField2.setPhColor(new java.awt.Color(255, 255, 255));
+        jCTextField2.setPlaceholder("Enter Passward");
+        jCTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCTextField2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jCTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 300, 330, -1));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pstu/hall/management/system/icons8_Secure_50px.png"))); // NOI18N
+        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 270, 50, 60));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pstu/hall/management/system/icons8_Account_50px.png"))); // NOI18N
+        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, 50, 60));
+
+        rSMaterialButtonCircle1.setText("Login");
+        rSMaterialButtonCircle1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rSMaterialButtonCircle1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(rSMaterialButtonCircle1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 360, 240, 70));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 0, 510, 600));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pstu/hall/management/system/rsz_pstu1.jpg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1070, 600));
+
+        setSize(new java.awt.Dimension(1070, 624));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        System.exit(0);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jCTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCTextField1ActionPerformed
+        
+    }//GEN-LAST:event_jCTextField1ActionPerformed
+
+    private void jCTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCTextField2ActionPerformed
+        
+    }//GEN-LAST:event_jCTextField2ActionPerformed
+
+    private void rSMaterialButtonCircle1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rSMaterialButtonCircle1ActionPerformed
+        
+        if(jCTextField1.getText().equals("afridi") && jCTextField2.getText().equals("afridi")){
+            setVisible(false);
+            new Home().setVisible(true);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Incorrect Username Or Passward");
+        }
+    }//GEN-LAST:event_rSMaterialButtonCircle1ActionPerformed
+
+    
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login_page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login_page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login_page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login_page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login_page().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private app.bolivia.swing.JCTextField jCTextField1;
+    private app.bolivia.swing.JCTextField jCTextField2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel2;
+    private rojerusan.RSMaterialButtonCircle rSMaterialButtonCircle1;
+    // End of variables declaration//GEN-END:variables
+}
